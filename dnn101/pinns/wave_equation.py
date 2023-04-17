4d7d6acb41446fa91a9c4818ed89f66a69e6e509
elizabethnewman <39045034+elizabethnewman@users.noreply.github.com>
2023-04-17 11:02:30 -0400

    added wave equation (in progress)

--- dnn101/pinns/wave_equation.py
+++ dnn101/pinns/wave_equation.py
@@ -1,4 +1,3 @@
-
 import torch
 from dnn101.utils import DNN101Data
 from typing import Callable
@@ -12,23 +11,28 @@ from dnn101.utils import DNN101Data
 from dnn101.pinns import PDEDomain, PDEDomainBox
 
 
+class WaveEquationPINN(torch.nn.Module):
 
-class HeatEquation1DPINN(torch.nn.Module):
+    def __init__(self, net):
+        super(WaveEquationPINN, self).__init__()
 
-    def __init__(self, net, x_domain, t_domain):
-        super(HeatEquation1DPINN, self).__init__()
         self.net = net
+        self.c = torch.nn.Parameter(torch.tensor(1))  # learnable parameter
 
-    def forward(self, xt_int, f, xt_init, g, xt_bound, b):
+    def forward(self, xt_int, f, xt_init, g, g_deriv):
         # evaluate initial condition
         n_init = xt_init.shape[0]
         pred_init = self.net(xt_init)
-        loss_init = (1 / n_init) * torch.norm(pred_init - g) ** 2
+        loss_init = (0.5 / n_init) * torch.norm(pred_init - g) ** 2
 
-        # evaluate boundary condition
-        n_b = xt_bound.shape[0]
-        pred_bound = self.net(xt_bound)
-        loss_bound = (0.5 / n_b) * (torch.norm(pred_bound - b) ** 2)
+        # evaluate initial conditions for derivative
+        x = xt_init[:, 0:1]
+        t = xt_init[:, 1:2]
+        x.requires_grad = True
+        t.requires_grad = True
+
+        pred_init_deriv = self.Neumann_initial(x, t)
+        loss_init_deriv = (0.5 / n_init) * torch.norm(pred_init_deriv - g_deriv) ** 2
 
         # evaluate physics and ensure we can differentiate
         n_int = xt_int.shape[0]
@@ -39,7 +43,16 @@ class HeatEquation1DPINN(torch.nn.Module):
         pred_f = self.pde(x, t)
         loss_f = (0.5 / n_int) * torch.norm(pred_f - f) ** 2
 
-        return loss_init, loss_bound, loss_f
+        return loss_init, loss_init_deriv, loss_f
+
+    def Neumann_initial(self, x, t):
+        u = self.net(torch.cat((x, t), dim=1))
+
+        u_t = torch.autograd.grad(u, t,
+                                  grad_outputs=torch.ones_like(u),
+                                  retain_graph=True,
+                                  create_graph=True)[0]
+        return u_t
 
     def pde(self, x, t):
 
@@ -49,6 +62,11 @@ class HeatEquation1DPINN(torch.nn.Module):
                                   grad_outputs=torch.ones_like(u),
                                   retain_graph=True,
                                   create_graph=True)[0]
+
+        u_tt = torch.autograd.grad(u_t, t,
+                                   grad_outputs=torch.ones_like(u),
+                                   retain_graph=True,
+                                   create_graph=True)[0]
 
         u_x = torch.autograd.grad(u, x,
                                   grad_outputs=torch.ones_like(u),
@@ -60,16 +78,16 @@ class HeatEquation1DPINN(torch.nn.Module):
                                    retain_graph=True,
                                    create_graph=True)[0]
 
-        return u_t - u_xx
+        return u_tt - (self.c ** 2) * u_xx
 
 
-class DNN101DataPINNHeatEquation1D(DNN101Data):
-    def __init__(self, f: Callable, g: Callable, g_init: Callable, domain: PDEDomain = PDEDomainBox((-1, 1, -1, 1)), u_true: Callable = None):
-        super(DNN101DataPINNHeatEquation1D, self).__init__()
+class DNN101DataPINNWaveEquation1D(DNN101Data):
+    def __init__(self, f: Callable, g_init: Callable, g_init_deriv: Callable, domain: PDEDomain = PDEDomainBox((-1, 1, -1, 1)), u_true: Callable = None):
+        super(DNN101DataPINNWaveEquation1D, self).__init__()
         self.f = f
-        self.g = g
         self.g_init = g_init
-        self.domain = domain # [x_low, x_high, t_low, t_high]
+        self.g_init_deriv = g_init_deriv
+        self.domain = domain  # [x_low, x_high, t_low, t_high]
         self.u_true = u_true
 
     def generate_data(self, n_train=80, n_val=10, n_test=10, p_bd=0.2, p_init=0.2):
@@ -82,7 +100,6 @@ class DNN101DataPINNHeatEquation1D(DNN101Data):
         # true right-hand side (source term)
         f = self.f(xt_int).view(-1, 1)
 
-
         # split into training and testing
         (x_int_train, f_train), (x_int_val, f_val), (x_int_test, f_test) = self.split_data(xt_int, f, n_train, n_val)
 
@@ -90,28 +107,21 @@ class DNN101DataPINNHeatEquation1D(DNN101Data):
         n_bd = math.floor(p_bd * n_int)
         xt_bd = self.domain.generate_boundary_points(n_bd)
         xt_init = xt_bd[:xt_bd.shape[0] // 4]
-        xt_bd = xt_bd[xt_bd.shape[0] // 2:]
-        g = self.g(xt_bd).view(-1, 1)
-
-        n_tot = xt_bd.shape[0]
-        p_train = n_train / n_int
-        p_val = n_val / n_int
-
-        (x_bd_train, g_train), (x_bd_val, g_val), (x_bd_test, g_test) = self.split_data(xt_bd, g, max(10, math.floor(p_train * n_tot)), max(10, math.floor(p_val * n_val)))
-
-
-        # true initial condition
-        g_init = self.g_init(xt_init)
+        g_init = self.g_init(xt_init).view(-1, 1)
 
         n_tot = xt_init.shape[0]
         p_train = n_train / n_int
         p_val = n_val / n_int
+        xg_init_train, xg_init_val, xg_init_test = self.split_data(xt_init, g_init, max(10, math.floor(p_train * n_tot)), max(10, math.floor(p_val * n_val)))
 
-        (x_init_train, g_init_train), (x_init_val, g_init_val), (x_init_test, g_init_test) = self.split_data(xt_init, g_init, max(10, math.floor(p_train * n_tot)), max(10, math.floor(p_val * n_val)))
+        xt_bd = self.domain.generate_boundary_points(n_bd)
+        xt_init = xt_bd[:xt_bd.shape[0] // 4]
+        g_init_deriv = self.g_init_deriv(xt_bd).view(-1, 1)
+        xg_init_deriv_train, xg_init_deriv_val, xg_init_deriv_test = self.split_data(xt_init, g_init_deriv, max(10, math.floor(p_train * n_tot)), max(10, math.floor(p_val * n_val)))
 
-        data_train = ((x_int_train, f_train), (x_bd_train, g_train), (x_init_train, g_init_train))
-        data_val = ((x_int_val, f_val), (x_bd_val, g_val), (x_init_val, g_init_val))
-        data_test = ((x_int_test, f_test), (x_bd_test, g_test), (x_init_test, g_init_test))
+        data_train = ((x_int_train, f_train), xg_init_train, xg_init_deriv_train)
+        data_val = ((x_int_val, f_val), xg_init_val, xg_init_deriv_val)
+        data_test = ((x_int_test, f_test), xg_init_test, xg_init_deriv_test)
         return data_train, data_val, data_test
 
     def plot_data(self, *args, label='train', marker='o'):
@@ -168,26 +178,23 @@ class DNN101DataPINNHeatEquation1D(DNN101Data):
                 plt.title('approx')
 
 
-def pde_libraryHeatEquation1D(fctn_num=0):
+def pde_libraryWaveEquation1D(fctn_num=0):
 
     if fctn_num == 0:
-        x_part = lambda x: torch.exp(-x ** 2 + 1) - 1
-        dx_part = lambda x: -2 * x * torch.exp(-x ** 2 + 1)
-        dx2_part = lambda x: (-2 + 4 * x) * torch.exp(-x ** 2 + 1)
-        t_part = lambda t: (t + 1) ** (-1 / 2)
-        dt_part = lambda t: -(t + 1) ** (-3 / 2)
+        f = lambda xt: 0 * xt[:, 0]
+        g_init = lambda xt: 0
 
-        u_true = lambda xt: x_part(xt[:, 0]) * t_part(xt[:, 1])  # true solution (unknown in practice)
-        f_true = lambda xt: x_part(xt[:, 0]) * dt_part(xt[:, 1]) - dx2_part(xt[:, 0]) * t_part(xt[:, 1])  # source term (in this case, f = du / dt - d^2u/dx^2)
+        # u_true = lambda xt: x_part(xt[:, 0]) * t_part(xt[:, 1])  # true solution (unknown in practice)
+        # f_true = lambda xt: x_part(xt[:, 0]) * dt_part(xt[:, 1]) - dx2_part(xt[:, 0]) * t_part(xt[:, 1])  # source term (in this case, f = du / dt - d^2u/dx^2)
         # g_true = lambda xt: x_part(xt[:, 0]) * t_part(t_domain[0])  # initial condition
         # b_true_0 = lambda xt: x_part(x_domain[0]) * t_part(xt[:, 1])  # Dirichlet boundary condition
         # b_true_1 = lambda xt: x_part(x_domain[1]) * t_part(xt[:, 1])  # Dirichlet boundary condition
-
-        pde = {'eqn': 'du/dt - d^2u/dx^2 = f; u_{bd} = g; u_{init} = g_init',
-               'u_true': u_true,
-               'f': f_true,
-               'g': u_true,
-               'g_init': u_true}
+        pde = None
+        # pde = {'eqn': 'du/dt - d^2u/dx^2 = f; u_{bd} = g; u_{init} = g_init',
+        #        'u_true': u_true,
+        #        'f': f_true,
+        #        'g': u_true,
+        #        'g_init': u_true}
     else:
         raise ValueError('fctn_num = 0 for Heat Equation1D')
 
@@ -197,13 +204,13 @@ def pde_libraryHeatEquation1D(fctn_num=0):
 if __name__ == "__main__":
     import torch.nn as nn
 
-    pde_setup = pde_libraryHeatEquation1D(0)
+    pde_setup = pde_libraryWaveEquation1D(0)
     f_true = pde_setup['f']
     g_true = pde_setup['g']
     g_init = pde_setup['g_init']
     u_true = pde_setup['u_true']
 
-    pde = DNN101DataPINNHeatEquation1D(f_true, g_true, g_init, domain=PDEDomainBox((-1, 1, 0, 10)), u_true=u_true)
+    pde = DNN101DataPINNWaveEquation1D(f_true, g_true, g_init, domain=PDEDomainBox((-1, 1, 0, 10)), u_true=u_true)
     (train_int, train_bd, train_init), (val_int, val_bd, val_init), (test_int, test_bd, test_init) = pde.generate_data()
     pde.plot_data(*train_int, *train_bd, *train_init, marker='o', label='train')
     pde.plot_data(*val_int, *val_bd, *val_init, marker='s', label='val')
