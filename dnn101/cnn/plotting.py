e65927785a25dcd835cc4f569d775ef8045a6aed
elizabethnewman <39045034+elizabethnewman@users.noreply.github.com>
2023-04-17 14:03:27 -0400

    Update plotting.py

--- dnn101/cnn/plotting.py
+++ dnn101/cnn/plotting.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import math
 
@@ -17,24 +18,32 @@ def montage_array(A, num_col=None, cmap='viridis', names=None):
 
     num_row = math.ceil(m3 / num_col)
 
-    C = torch.zeros([m1 * num_row, m2 * num_col])
+    C = torch.nan * torch.ones([m1 * num_row + num_row - 1, m2 * num_col + num_col - 1])
+
+    cmap = mpl.cm.viridis
+    cmap.set_bad('black', 0.)
 
     k = 0
-    for p in range(0, C.shape[0], m1):
-        for q in range(0, C.shape[1], m2):
+    shift_p = 0
+    for p in range(0, num_row):
+        shift_q = 0
+        for q in range(0, num_col):
             if k >= m3:
-                C[p:p + m1, q:q + m2] = torch.nan
+                # C[p * m1 + shift_p:(p + 1) * m1 + shift_p, q * m2 + shift_q:(q + 1) * m2 + shift_q] = torch.nan
                 break
-            C[p:p + m1, q:q + m2] = A[:, :, k]
+            C[p * m1 + shift_p:(p + 1) * m1 + shift_p, q * m2 + shift_q:(q + 1) * m2 + shift_q] = A[:, :, k]
             k += 1
+            shift_q += 1
+        shift_p += 1
+        
 
     img = plt.imshow(C, cmap=cmap)
 
     # grid lines
-    for i in range(0, C.shape[0] + 1, m1):
-        plt.hlines(y=i-0.5, xmin=-0.5, xmax=C.shape[0]-0.5, linewidth=8, color='w')
-    for i in range(0, C.shape[1] + 1, m2):
-        plt.vlines(x=i-0.5, ymin=-0.5, ymax=C.shape[1]-0.5, linewidth=8, color='w')
+    # for i in range(0, C.shape[0] + 1, m1):
+    #     plt.hlines(y=i-0.5, xmin=-0.5, xmax=C.shape[0]-0.5, linewidth=8, color='w')
+    # for i in range(0, C.shape[1] + 1, m2):
+    #     plt.vlines(x=i-0.5, ymin=-0.5, ymax=C.shape[1]-0.5, linewidth=8, color='w')
 
     plt.axis('off')
     # cb = plt.colorbar()
